58a8e24769436eaaa66271c023fae97379276b8d
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-08-22 12:38:35 +0200

    update method

--- src/main/java/com/example/Animal/Shelter/controllers/PetsController.java
+++ src/main/java/com/example/Animal/Shelter/controllers/PetsController.java
@@ -1,5 +1,6 @@
 package com.example.Animal.Shelter.controllers;
 
+import com.example.Animal.Shelter.models.Pets;
 import com.example.Animal.Shelter.services.PetsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +12,8 @@ public class PetsController {
 
   @Autowired PetsService petsService;
 
-  @DeleteMapping(path = "/pets/{id}")
-  public void deletePets(@PathVariable Integer id) {
-    petsService.deletePets(id);
+  @PutMapping(path="/pets/{id}")
+  public void updatePets(@RequestBody Pets pets, @PathVariable Integer id){
+    petsService.updatePets(pets, id);
   }
 }
--- src/main/java/com/example/Animal/Shelter/services/PetsService.java
+++ src/main/java/com/example/Animal/Shelter/services/PetsService.java
@@ -1,5 +1,6 @@
 package com.example.Animal.Shelter.services;
 
+import com.example.Animal.Shelter.models.Pets;
 import com.example.Animal.Shelter.repositories.IPetsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,8 @@ public class PetsService {
 
   @Autowired IPetsRepository iPetsRepository;
 
-  public void deletePets(Integer id) {
-    iPetsRepository.deleteById(id);
+  public void updatePets(Pets pets, Integer id){
+    pets.setId(id);
+    iPetsRepository.save(pets);
   }
 }

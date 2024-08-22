af81b8689491d10b9d5fc26ce66ce8a0313aaf50
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-08-22 14:39:30 +0200

    made create method

--- src/main/java/com/example/Animal/Shelter/services/DonationsService.java
+++ src/main/java/com/example/Animal/Shelter/services/DonationsService.java
@@ -1,5 +1,6 @@
 package com.example.Animal.Shelter.services;
 
+import com.example.Animal.Shelter.models.Donations;
 import com.example.Animal.Shelter.repositories.IDonationsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,10 @@ public class DonationsService {
 
     @Autowired
     IDonationsRepository iDonationsRepository;
+
+    public Donations createDonations(Donations newDonations) {
+        return iDonationsRepository.save(newDonations);
+    }
 
 
 }

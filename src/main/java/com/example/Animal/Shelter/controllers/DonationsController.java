c6f062363c611c12e4c3f3c60cf68d2ffa7dfdd3
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-08-22 15:02:01 +0200

    made get methods

--- src/main/java/com/example/Animal/Shelter/controllers/DonationsController.java
+++ src/main/java/com/example/Animal/Shelter/controllers/DonationsController.java
@@ -2,8 +2,14 @@ package com.example.Animal.Shelter.controllers;
 
 import com.example.Animal.Shelter.models.Donations;
 import com.example.Animal.Shelter.services.DonationsService;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +28,13 @@ public class DonationsController {
         return donationsService.createDonations(donations);
     }
 
+    @GetMapping(path = "/Donations")
+    public List<Donations> getAllDonations() {
+        return donationsService.getAllDonations();
+    }
 
-}
+    @GetMapping(path = "/Donations/{id}")
+        public Optional<Donations> getDonationsbyId(@PathVariable int id) {
+            return donationsService.getDonationsbyId(id);
+        }
+    }
--- src/main/java/com/example/Animal/Shelter/services/DonationsService.java
+++ src/main/java/com/example/Animal/Shelter/services/DonationsService.java
@@ -2,6 +2,10 @@ package com.example.Animal.Shelter.services;
 
 import com.example.Animal.Shelter.models.Donations;
 import com.example.Animal.Shelter.repositories.IDonationsRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +17,15 @@ public class DonationsService {
 
     public Donations createDonations(Donations newDonations) {
         return iDonationsRepository.save(newDonations);
+    }
+
+    public List<Donations> getAllDonations() {
+        return (List<Donations>) iDonationsRepository.findAll();
+    }
+
+    public Optional<Donations> getDonationsbyId(int id) {
+        Donations donations = iDonationsRepository.findById(id).orElseThrow();
+        return Optional.of(donations);
     }
 
 

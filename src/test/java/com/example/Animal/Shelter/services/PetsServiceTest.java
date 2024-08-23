165f95edb18e524822e2e822e74d536a4dcdd8fa
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-08-23 09:36:54 +0200

    setup test

--- src/test/java/com/example/Animal/Shelter/services/PetsServiceTest.java
+++ src/test/java/com/example/Animal/Shelter/services/PetsServiceTest.java
@@ -1,28 +1,78 @@
 package com.example.Animal.Shelter.services;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.Animal.Shelter.models.Pets;
+import com.example.Animal.Shelter.repositories.IPetsRepository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 class PetsServiceTest {
+  @Mock private IPetsRepository iPetsRepository;
+  @InjectMocks private PetsService petsService;
 
-    @Test
-    void createPets() {
-    }
+  private Pets petLolo;
+  private Pets petPitu;
 
-    @Test
-    void getAllPets() {
-    }
+  private List<Pets> petsList = new ArrayList<>();
 
-    @Test
-    void getPetsById() {
-    }
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+    Pets petLolo = new Pets();
+    petLolo.setId(1);
+    petLolo.setName("Lolo");
+    petLolo.setPicture("https://media.traveler.es/photos/613760adcb06ad0f20e11980/master/w_1600,c_limit/202931.jpg");
+    petLolo.setAnimalType("Dog");
+    petLolo.setRace("Rottweiler");
+    petLolo.setBirthDate(LocalDate.of(2024,8, 17);
+    petLolo.setGender(false);
+    petLolo.setStatus(false);
+    petLolo.setSterilized(false);
+    petLolo.setTimeInTheShelter(LocalDate.of(2024,9,5));
+    petLolo.setDescription("Very affectionate and playful puppy");
 
-    @Test
-    void updatePets() {
-    }
+    Pets petPitu = new Pets();
+    petPitu.setId(2);
+    petPitu.setName("Pitu");
+    petLolo.setPicture("https://img2.rtve.es/i/?w=1600&i=1618587961630.jpg");
+    petLolo.setAnimalType("Cat");
+    petLolo.setRace("Orange tabby");
+    petLolo.setBirthDate(LocalDate.of(2022,4, 18);
+    petLolo.setGender(false);
+    petLolo.setStatus(false);
+    petLolo.setSterilized(true);
+    petLolo.setTimeInTheShelter(LocalDate.of(2023,5,5));
+    petLolo.setDescription("Playful cat");
 
-    @Test
-    void deletePets() {
-    }
+    petsList.add(petLolo);
+    petsList.add(petPitu);
+  }
+
+      @Test
+      void createPets() {
+      }
+
+      @Test
+      void getAllPets() {
+      }
+
+      @Test
+      void getPetsById() {
+      }
+
+      @Test
+      void updatePets() {
+      }
+
+      @Test
+      void deletePets() {
+      }
 }

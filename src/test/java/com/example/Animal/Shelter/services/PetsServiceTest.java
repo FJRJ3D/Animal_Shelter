ed38d302a5e0820db71c8323d9d31bdca0a30667
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-08-23 09:02:56 +0200

    added tests packages

---
+++ src/test/java/com/example/Animal/Shelter/services/PetsServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.Animal.Shelter.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetsServiceTest {
+
+    @Test
+    void createPets() {
+    }
+
+    @Test
+    void getAllPets() {
+    }
+
+    @Test
+    void getPetsById() {
+    }
+
+    @Test
+    void updatePets() {
+    }
+
+    @Test
+    void deletePets() {
+    }
+}

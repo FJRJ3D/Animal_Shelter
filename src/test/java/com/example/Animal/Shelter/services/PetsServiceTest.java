af7e7ebae092d5da42a1cc6f1ac4306d5f66c580
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-08-23 11:15:59 +0200

    added createServiceTest

--- src/test/java/com/example/Animal/Shelter/services/PetsServiceTest.java
+++ src/test/java/com/example/Animal/Shelter/services/PetsServiceTest.java
@@ -64,7 +64,17 @@ class PetsServiceTest {
 
           Pets result = petsService.createPets(petLolo);
 
+          assertEquals(1, result.getId());
+          assertEquals("Lolo", result.getName());
+          assertEquals("https://media.traveler.es/photos/613760adcb06ad0f20e11980/master/w_1600,c_limit/202931.jpg", result.getPicture());
           assertEquals("Dog", result.getAnimalType());
+          assertEquals("Rottweiler", result.getRace());
+          assertEquals(LocalDate.of(2024,8, 17), result.getBirthDate());
+          assertFalse(result.isGender());
+          assertFalse(result.isStatus());
+          assertFalse(result.isSterilized());
+          assertEquals(LocalDate.of(2024,9,5), result.getTimeInTheShelter());
+          assertEquals("Very affectionate and playful puppy", result.getDescription());
       }
 
       @Test

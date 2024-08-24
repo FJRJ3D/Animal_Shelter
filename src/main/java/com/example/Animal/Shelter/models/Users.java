b1b4a1b0e5b71cf5ae3c27f476acd071060aa8e9
jmat732 <jmatisamuel@gmail.com>
2024-08-24 10:53:00 +0200

    Cofig and mapped User BD an rol

--- src/main/java/com/example/Animal/Shelter/models/Users.java
+++ src/main/java/com/example/Animal/Shelter/models/Users.java
@@ -1,6 +1,17 @@
 package com.example.Animal.Shelter.models;
 
-import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,4 +37,12 @@ public class Users {
 
     @Column(name = "Adopted Animals")
     private String adoptedAnimals;
+    
+    @Column(name = "Role")
+    private String role;
+
+    @OneToMany(mappedBy = "User", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Pets> pets = new ArrayList<Pets>(); 
+
+
 }

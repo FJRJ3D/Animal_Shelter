da14a261a7bd8a8ded5f2d09637e5d03d0de8ed6
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-08-26 22:08:04 +0200

    update getAll integrationTest

--- src/test/java/com/example/Animal/Shelter/controllers/PetsControllerTest.java
+++ src/test/java/com/example/Animal/Shelter/controllers/PetsControllerTest.java
@@ -1,8 +1,6 @@
 package com.example.Animal.Shelter.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -10,7 +8,6 @@ import com.example.Animal.Shelter.models.Pets;
 import com.example.Animal.Shelter.services.PetsService;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -77,18 +74,34 @@ class PetsControllerTest {
     mockMvc.perform(MockMvcRequestBuilders.get("/api/as/pets"))
             .andExpect(status().isOk())
             .andExpect(content().json(
-           "[{\"id\": 1,\n"
-                   +"\"name\": \"Lolo\",\n"
-                   +"\"picture\": \"https://media.traveler.es/photos/613760adcb06ad0f20e11980/master/w_1600,c_limit/" +
-                   "202931.jpg\",\n"
-                   +"\"animalType\": \"Dog\",\n"
-                   +"\"race\": \"Rottweiler\",\n"
-                   +"\"gender\": false,\n"
-                   +"\"status\": false,\n"
-                   +"\"birthDate\": \"17-08-2024\",\n"
-                   +"\"sterilized\": false,\n"
-                   +"\"timeInTheShelter\": \"05-09-2024\",\n"
-                   +"\"description\": \"Very affectionate and playful puppy\"}," +
+           "[\n" +
+                   "    {\n" +
+                   "        \"id\": 1,\n" +
+                   "        \"name\": \"Lolo\",\n" +
+                   "        \"picture\": \"https://media.traveler.es/photos/613760adcb06ad0f20e11980/master/" +
+                   "w_1600,c_limit/202931.jpg\",\n" +
+                   "        \"animalType\": \"Dog\",\n" +
+                   "        \"race\": \"Rottweiler\",\n" +
+                   "        \"gender\": false,\n" +
+                   "        \"status\": false,\n" +
+                   "        \"birthDate\": \"17-08-2024\",\n" +
+                   "        \"sterilized\": false,\n" +
+                   "        \"timeInTheShelter\": \"05-09-2024\",\n" +
+                   "        \"description\": \"Very affectionate and playful puppy\"\n" +
+                   "    },\n" +
+                   "    {\n" +
+                   "        \"id\": 2,\n" +
+                   "        \"name\": \"Pitu\",\n" +
+                   "        \"picture\": \"https://img2.rtve.es/i/?w=1600&i=1618587961630.jpg\",\n" +
+                   "        \"animalType\": \"Cat\",\n" +
+                   "        \"race\": \"Orange tabby\",\n" +
+                   "        \"gender\": false,\n" +
+                   "        \"status\": false,\n" +
+                   "        \"birthDate\": \"18-04-2022\",\n" +
+                   "        \"sterilized\": true,\n" +
+                   "        \"timeInTheShelter\": \"05-05-2023\",\n" +
+                   "        \"description\": \"Playful cat\"\n" +
+                   "    }\n" +
                    "]"));
   }
 

a23007a89b3c7685a013212ebdd747dd95ba2c57
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-08-26 09:26:24 +0200

    merge create, getAll and getById service tests

--- src/test/java/com/example/Animal/Shelter/services/PetsServiceTest.java
+++ src/test/java/com/example/Animal/Shelter/services/PetsServiceTest.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;

9bbd0b7d226cb47963203496c562f9cd309fb3c6
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-08-27 23:20:03 +0200

    added some test

---
+++ src/test/java/com/example/Animal/Shelter/services/DonationsServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.Animal.Shelter.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+import com.example.Animal.Shelter.models.Pets;
+import com.example.Animal.Shelter.repositories.IPetsRepository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class DonationsServiceTest {
+
+}

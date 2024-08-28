be5b157690a287db06afda4adba635cae9c7f0d1
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-08-28 12:32:37 +0200

    changes

--- src/main/java/com/example/Animal/Shelter/AnimalShelterApplication.java
+++ src/main/java/com/example/Animal/Shelter/AnimalShelterApplication.java
@@ -2,9 +2,8 @@ package com.example.Animal.Shelter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class AnimalShelterApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/example/Animal/Shelter/models/Users.java
+++ src/main/java/com/example/Animal/Shelter/models/Users.java
@@ -1,17 +1,9 @@
 package com.example.Animal.Shelter.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,24 +17,27 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Users {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
 
-    @Column(name = "Name")
-    private String name;
+  @Column(name = "Name")
+  private String name;
 
-    @Column(name = "Password")
-    private String password;
+  @Column(name = "Password")
+  private String password;
 
-    @Column(name = "Adopted Animals")
-    private String adoptedAnimals;
-    
-    @Column(name = "Role")
-    private String role;
+  @Column(name = "Adopted Animals")
+  private String adoptedAnimals;
 
-    @OneToMany(mappedBy = "User", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    private List<Pets> pets = new ArrayList<Pets>(); 
+  @Column(name = "Role")
+  private String role;
 
-
+  @OneToMany(
+      mappedBy = "users",
+      cascade = CascadeType.ALL,
+      orphanRemoval = true,
+      fetch = FetchType.LAZY)
+  @JsonManagedReference
+  private List<Pets> pets = new ArrayList<>();
 }
